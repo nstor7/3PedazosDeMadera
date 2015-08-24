e5ef35a49c00c5606116bd882077134e60b64ad8
Néstor Ibarra <nstor7@hotmail.com>
2015-08-24 13:19:16 -0500

    Segundo commit
    Creación del servidor, ruteo a la carpeta página.
    formato de la index html
    colocación del logo
    inclusión de normalize
    compilación de stylus
    diseño del header
    principio del diseño del navegador

---
+++ servidor.js
@@ -0,0 +1,12 @@
+console.log('Arrancando el servidor de 3PM');
+var express = require('express');
+var parcero = require('body-parser');
+var app = express();
+app.use(parcero.urlencoded({extended:true}));
+var servidor;
+
+servidor = app.listen(8080, function(){
+    console.log('Servidor Arrancado')
+});
+
+app.use(express.static('pagina'));
